b678e61f61d8d89d219908fd544ee81f1a8a6628
Maxim Nefedov <max-ipad@mail.ru>
2021-01-17 14:10:52 +0300

    Add CreditCalculator realisation

--- src/main/java/TDD/CreditCalculator.java
+++ src/main/java/TDD/CreditCalculator.java
@@ -5,22 +5,25 @@ public class CreditCalculator {
     private double sum;
     private double interestRate;
     private double creditTerm;
+    private double rate;
 
     public CreditCalculator(double sum, double interestRate, double creditTerm) {
         this.sum = sum;
         this.interestRate = interestRate;
         this.creditTerm = creditTerm;
+        this.rate = interestRate / 1200.0;
     }
 
     public double calcMonthlyPayment() {
-        return 0.0;
+        return Math.abs(sum * rate /
+                (1.0 - Math.pow(1.0 + (interestRate / 1200.0), creditTerm)));
     }
 
     public double calcOverpayment() {
-        return 0.0;
+        return Math.abs((creditTerm * calcMonthlyPayment()) - sum);
     }
 
     public double totalAmountToBeReturned() {
-        return 0.0;
+        return sum + calcOverpayment();
     }
 }
--- src/main/java/TDD/Main.java
+++ src/main/java/TDD/Main.java
@@ -1,7 +1,23 @@
 package TDD;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите сумму кредита");
+        int sum = scanner.nextInt();
+        System.out.println("Введите процентную ставку");
+        int interestRate = scanner.nextInt();
+        System.out.println("Введите срок кредита (мес)");
+        int creditTerm = scanner.nextInt();
 
+        CreditCalculator creditCalculator = new CreditCalculator(sum, interestRate, creditTerm);
+        double monthlyPayment = creditCalculator.calcMonthlyPayment();
+        double overpayment = creditCalculator.calcOverpayment();
+        double amountToBeReturned = creditCalculator.totalAmountToBeReturned();
+        System.out.printf("Ежемесячный палтеж: %.2f\n" +
+                "Переплата: %.2f\n" +
+                "Общая сумма к возврату: %.2f", monthlyPayment, overpayment, amountToBeReturned);
     }
 }
